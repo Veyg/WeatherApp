d239515bee332542dab0a5a89f910faead7e9993
Dawid Grochowski <96694367+Veyg@users.noreply.github.com>
2023-07-09 18:04:01 +0200

    .git ignore added

---
+++ src/main/java/com/weatherapp/WeatherApp.java
@@ -0,0 +1,9 @@
+package com.weatherapp;
+
+import java.io.IOException;
+
+public class WeatherApp {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello App");
+            }
+}
